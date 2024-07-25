7bdd8472cbaece7b7c6a884a9cc3f72fec86f483
Vijayadharshan <vsdharshan2006@gmail.com>
2024-07-25 20:10:09 +0530

    Added .sh file

---
+++ Periodic_table_Database/element.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  elements=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = '$1' ")
+else
+    elements=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$1' or name= '$1'")
+fi
+
+if [[ -z $elements ]]
+then 
+  echo "I could not find that element in the database."
+  exit
+fi
+
+
+echo $elements | while IFS="|" read atomic_number name symbol type atomic_mass melting_point boiling_point
+do  
+  echo -e "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+done
